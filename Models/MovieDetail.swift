3511ca11649639e5fd3e29a6eb5500bc3fef13b1
arinasilchenko <silchenkoarisha@gmail.com>
2026-06-20 17:50:21 +0300

    FEATURE1: Add API request for details screen, change UI

---
+++ Models/MovieDetail.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+struct MovieDetail: Decodable {
+    let title: String
+    let year: String
+    let plot: String        // описание
+    let director: String    // режиссёры (строкой через запятую)
+    let actors: String      // актёры (строкой через запятую)
+    let imdbRating: String  // рейтинг, например "7.5" или "N/A"
+    let poster: String
+    let response: String
+    let error: String?
+
+    enum CodingKeys: String, CodingKey {
+        case title = "Title"
+        case year = "Year"
+        case plot = "Plot"
+        case director = "Director"
+        case actors = "Actors"
+        case imdbRating
+        case poster = "Poster"
+        case response = "Response"
+        case error = "Error"
+    }
+
+    var posterURL: URL? { poster == "N/A" ? nil : URL(string: poster) }
+}
--- ShowBill/MovieDetailsController.swift
+++ ShowBill/MovieDetailsController.swift
@@ -1,8 +1,13 @@
 import UIKit
 
 final class MovieDetailsController: UIViewController {
+    
+    // MARK: - Public Properties
+    var movie: Movie?
 
     // MARK: - Private Properties
+    
+    private let loader = MoviesLoader()
 
     private lazy var movieImage: UIImageView = {
         let image = UIImageView()
@@ -12,14 +17,25 @@ final class MovieDetailsController: UIViewController {
         return image
     }()
 
-    private lazy var movieTitle: UILabel = {
-        let label = UILabel()
-        label.font = .preferredFont(forTextStyle: .title1)
-        label.textAlignment = .center
-        label.lineBreakMode = .byTruncatingTail
-        return label
+    private lazy var movieTitle = makeLabel(style: .largeTitle)
+    private lazy var descriptionLabel = makeLabel(style: .body)
+    private lazy var directorLabel = makeLabel(style: .subheadline)
+    private lazy var actorsLabel = makeLabel(style: .subheadline)
+    private lazy var ratingLabel = makeLabel(style: .title3)
+    
+    private lazy var watchButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Буду смотреть", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.titleLabel?.font = .preferredFont(forTextStyle: .headline)
+        button.backgroundColor = .purple
+        button.layer.cornerRadius = 16
+        button.clipsToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.addTarget(self, action: #selector(watchTapped), for: .touchUpInside)
+        return button
     }()
-
+    
     // TODO: - UIViewController Lifecycle
 
     override func viewDidLoad() {
@@ -27,36 +43,86 @@ final class MovieDetailsController: UIViewController {
         view.accessibilityIdentifier = "movieDetailsView"
 
         setupLayout()
+        guard let movie else { return }
+        movieTitle.text = movie.title
+        loadDetails(for: movie.imdbID)
     }
-
-    // MARK: Public Methods
-
-    public func configure(image: UIImage?, title: String?) {
+    
+    // MARK: - Public Methods
+    public func configure(image: UIImage?) {
         movieImage.image = image
-        movieTitle.text = title
     }
 
     // MARK: - Private Methods
-
+    private func loadDetails(for imbdID: String) {
+        loader.loadDetail(imdbID: imbdID) { [weak self] result in
+            DispatchQueue.main.async {
+                guard case .success(let d) = result else { return }
+                self?.movieTitle.text = d.title
+                self?.descriptionLabel.text = d.plot
+                self?.directorLabel.text = "Режиссёр: \(d.director)"
+                self?.actorsLabel.text = "В ролях: \(d.actors)"
+                self?.ratingLabel.text = "Рейтинг: \(d.imdbRating)"
+                self?.loadPoster(from: d.posterURL)
+            }
+        }
+    }
+    
+    private func loadPoster(from url: URL?) {
+        guard let url else { return }
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+            guard let data, let image = UIImage(data: data) else { return }
+            DispatchQueue.main.async { self?.movieImage.image = image }
+        }.resume()
+    }
+    
+    private func makeLabel(style: UIFont.TextStyle) -> UILabel {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: style)
+        label.numberOfLines = 0
+        return label
+    }
+    
     private func setupLayout() {
         view.backgroundColor = .systemBackground
-
-        view.addSubview(movieImage)
-        view.addSubview(movieTitle)
+        
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(scrollView)
+        
+        let stack = UIStackView(arrangedSubviews: [
+            movieImage, movieTitle, ratingLabel, directorLabel, actorsLabel, descriptionLabel, watchButton
+        ])
+        stack.axis = .vertical
+        stack.spacing = 12
+        stack.alignment = .fill
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(stack)
 
         movieImage.translatesAutoresizingMaskIntoConstraints = false
+        
         NSLayoutConstraint.activate([
-            movieImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            movieImage.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            movieImage.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16)
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            stack.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor, constant: 16),
+            stack.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor, constant: 16),
+            stack.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor, constant: -16),
+            stack.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor, constant: -16),
+            
+            stack.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor, constant: -32),
+            
+            movieImage.heightAnchor.constraint(equalToConstant: 280),
+            movieImage.widthAnchor.constraint(equalTo: movieImage.heightAnchor, multiplier: 2.0/3.0),
+            movieImage.centerXAnchor.constraint(equalTo: stack.centerXAnchor),
+            
+            watchButton.heightAnchor.constraint(equalToConstant: 52)
         ])
-
-        movieTitle.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            movieTitle.topAnchor.constraint(equalTo: movieImage.bottomAnchor, constant: 16),
-            movieTitle.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
-            movieTitle.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -16),
-            movieTitle.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -16)
-        ])
+    }
+    
+    @objc private func watchTapped() {
+        //TO-DO
     }
 }
